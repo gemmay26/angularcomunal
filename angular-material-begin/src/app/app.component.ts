0315a48d96d22414185127c089122a0e97280f1c
gemmay26 <gemmay26@gmail.com>
2020-09-19 12:23:04 -0500

    Proyecto de angular

--- angular-material-begin/src/app/app.component.ts
+++ angular-material-begin/src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-material-begin';
+  isavailable = true;
+  reservadesalas(event) {
+    //just added console.log which will display the event details in browser on click of the button.
+    alert("Usted a seleccionado reserva de salas");
+    console.log(event);
+  }
+  reportedeproblemas(event) {
+    //just added console.log which will display the event details in browser on click of the button.
+    alert("Usted a seleccionado Reporte de problemas");
+    console.log(event);
+  }
+  miequipo(event) {
+    //just added console.log which will display the event details in browser on click of the button.
+    alert("Usted a seleccionado Mi equipo");
+    console.log(event);
+  }
+  comunicados(event) {
+    //just added console.log which will display the event details in browser on click of the button.
+    alert("Usted a seleccionado Comunicados");
+    console.log(event);
+  }
+  fichasintomatologia(event) {
+    //just added console.log which will display the event details in browser on click of the button.
+    alert("Usted a seleccionado Ficha de sintomatologia");
+    console.log(event);
+  }
 }
